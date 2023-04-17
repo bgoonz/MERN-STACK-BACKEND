ecd551ee90faf7fb3ac0286e59fb950c84a48bed
Bryan C Guner <bryan.guner@gmail.com>
2023-04-17 11:16:48 -0400

    handeling errors ... in synchronus code

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use('/api/places',placesRoutes);// => /api/places/...
+app.use("/api/places", placesRoutes); // => /api/places/...
+
+// this error handling middleware will exicute if any middleware above it throws an error
+app.use((error, req, res, next) => {
+  //if response has already been sent
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 app.listen(5000);
