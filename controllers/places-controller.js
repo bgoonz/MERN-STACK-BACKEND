152e8e47e237ba874e8dffcb08e1dc9b9476fa2d
Bryan C Guner <bryan.guner@gmail.com>
2023-04-17 17:50:56 -0400

    google maps geolocation works... converts address into cordinates

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
 let DUMMY_PLACES = [
   {
     id: "p1",
@@ -39,14 +40,20 @@ const getPlacesByUserId = async (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
 
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    next(new HttpError("Invalid inputs passed, please check your data.", 422));
   }
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
   const createdPlace = {
     id: uuidv4(),
     title,
---
+++ util/location.js
@@ -0,0 +1,24 @@
+const HttpError = require("../models/http-error");
+
+const axios = require("axios");
+const API_KEY = "AIzaSyCqEAyCWnRM3KZTn0duEveWpW-JxZ19Sbs";
+async function getCoordsForAddress(address) {
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+      address
+    )}&key=${API_KEY}`
+  );
+  const data = response.data;
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422
+    );
+
+    throw error;
+  }
+  const coordinates = data.results[0].geometry.location;
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
