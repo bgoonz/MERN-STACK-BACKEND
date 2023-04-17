fbe359963684e3da5a7463e88118c39c3a1e34fc
Bryan C Guner <bryan.guner@gmail.com>
2023-04-17 12:04:51 -0400

    added error class model

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const HttpError = require("../models/http-error");
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -22,9 +22,7 @@ router.get("/:pid", (req, res, next) => {
     return p.id === placeId;
   });
   if (!place) {
-    const error = new Error("Could not find a place for the provided id.");
-    error.code = 404;
-    throw error;
+    throw new HttpError("Could not find a place for the provided id.", 404);
   }
 
   res.json({ place: place });
@@ -36,9 +34,9 @@ router.get("/user/:uid", (req, res, next) => {
     return p.creator === userId;
   });
   if (!place) {
-    const error = new Error("Could not find a place for the provided user-id.");
-    error.code = 404;
-    return next(error);
+    return next(
+      HttpError("Could not find a place for the provided user-id.", 404)
+    );
   }
   res.json({ place: place });
 });
