cbb67c39fe68b01b1c35cd4603a0b2a858dba38b
Bryan C Guner <bryan.guner@gmail.com>
2023-04-23 18:43:12 -0400

    backend route protection using auth middlewhere

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,9 +1,21 @@
-const HttpError = require( "../models/http-error" );
-module.exports = function ( req, res, next ) {
+const HttpError = require("../models/http-error");
+const jwt = require("jsonwebtoken");
+module.exports = function (req, res, next) {
+  try {
     //Authorization: 'Bearer TOKEN' ... split on space[1] gives us the token
-    const token = req.header.authorization.split( " " )[ 1 ];
-    if ( !token ) {
-        const error = new HttpError( "Authentication failed.", 401 );
+    const token = req.header.authorization.split(" ")[1];
+
+    if (!token) {
+      const error = new HttpError("Authentication failed.");
     }
-        
+    //validating the token
+    const decodedToken = jwt.verify(token, "supersecret_dont_share");
+    //add data to the request
+    req.userData = { userId: decodedToken.userId };
+    //let request continue
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication failed.", 401);
+    return next(error);
+  }
 };
