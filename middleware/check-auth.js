51d7743cc53a32cc838c1758b60ac061420fc17a
Bryan C Guner <bryan.guner@gmail.com>
2023-04-23 19:37:56 -0400

    bypassing the options request that proceeds a post request

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,9 +1,14 @@
-const HttpError = require("../models/http-error");
 const jwt = require("jsonwebtoken");
+
+const HttpError = require("../models/http-error");
 module.exports = function (req, res, next) {
+  if (req.method === "OPTIONS") {
+    // this will not necessarily allow the post request to continue but it will allow the options request to continue.
+    return next();
+  }
   try {
     //Authorization: 'Bearer TOKEN' ... split on space[1] gives us the token
-    const token = req.header.authorization.split(" ")[1];
+    const token = req.headers.authorization.split(" ")[1];
 
     if (!token) {
       const error = new HttpError("Authentication failed.");
