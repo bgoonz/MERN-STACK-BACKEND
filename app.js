b3432cc10061bd37dadf7663e0440fbc92e5d88c
Bryan C Guner <bryan.guner@gmail.com>
2023-04-23 15:42:56 -0400

    statically serving images using express

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
+const fs = require( "fs" );
+const path = require( "path" );
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
-const HttpError = require( "./models/http-error" );
-const fs = require( 'fs' );
+const HttpError = require("./models/http-error");
 
 const app = express();
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 
-app.use( ( req, res, next ) => {
-    res.setHeader( "Access-Control-Allow-Origin", "*" );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    res.setHeader( "Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE" );
-    next();
-} );
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
 app.use("/api/places", placesRoutes); // => /api/places/...
 app.use("/api/users", usersRoutes); // => /api/users/...
@@ -33,12 +35,12 @@ app.use((req, res, next) => {
 // this error handling middleware will exicute if any middleware above it throws an error
 app.use((error, req, res, next) => {
   //if response has already been sent
-    if ( req.file ) {
-        //if there is an error and a file was uploaded, delete the file
-        fs.unlink( req.file.path, ( err ) => {
-            console.log( err );
-        } );
-    }
+  if (req.file) {
+    //if there is an error and a file was uploaded, delete the file
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
   if (res.headerSent) {
     return next(error);
   }
